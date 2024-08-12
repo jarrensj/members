eb26689d7ff9bd6563bb8b36464f78f31f2f0aa9
Jarren San Jose <sanjose.jarren@gmail.com>
2024-08-11 19:51:33 -0700

    team

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1 className="text-2xl font-bold">hi</h1>
+      <h1 className="text-2xl font-bold">team</h1>
     </main>
   );
 }
